7dc1bcb368e4b2b43c7bf943980012130a7e30d2
Lotus Tea <lotustea@Lotuss-iMac.local>
2023-01-04 09:39:57 -0300

    feat: migrations, models and controllers for: brand, category and product; model relationships

--- database/migrations/2023_01_04_122335_create_categories_table.php
+++ database/migrations/2023_01_04_122335_create_categories_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->tinyInteger('type')->default(0)->comment("0 => User, 1 => Admin");
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
